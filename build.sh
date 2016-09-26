21ddcbae0f78baba071ed977188b7bbe205e9f83
Wiseluster <chinaware@foxmail.com>
2016-09-26 21:32:22 +0800

    Update scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+[ -f winlua.c ] && \
 /usr/bin/gcc -Wall -std=gnu11 -g -O2 winlua.c -o winlua.exe && \
 /usr/bin/strip winlua.exe
 
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-([ -f winlua.exe ] || ./build.sh) && \
+[ winlua.c -nt winlua.exe ] && ./build.sh && \
 [ -z "${prefix}" ] && prefix="/usr" && \
 /usr/bin/install -c -m 755 winlua.exe "${prefix}/bin"
 
