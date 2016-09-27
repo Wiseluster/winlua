d794389bb01d2a419a22ee78ab1f91b3481b1aa8
Wiseluster <chinaware@foxmail.com>
2016-09-27 14:42:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-[ winlua.c -ot winlua.exe ] || \
-./build.sh && \
+([ winlua.c -ot winlua.exe ] || \
+./build.sh) && \
 [ -z "${prefix}" ] && \
 prefix="/usr" && \
 /usr/bin/install -c -m 755 winlua.exe "${prefix}/bin"
